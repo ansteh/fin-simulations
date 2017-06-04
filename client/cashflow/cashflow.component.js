0bf67ee33ec4bacbf00da6a9d6eac786d8ea6b2e
ansteh <stehle.andre@googlemail.com>
2017-06-04 23:30:12 +0200

    cashflow chart

--- client/cashflow/cashflow.component.js
+++ client/cashflow/cashflow.component.js
@@ -1,12 +1,16 @@
 function CashflowController($scope, $element, $attrs, Cashflow) {
   var ctrl = this;
 
-  Cashflow.loadAssets()
-    .then(function() {
-      ctrl.assets = Cashflow.getAssets();
-    }, function(err) {
-      console.log(err);
-    });
+  ctrl.$onInit = function() {
+    Cashflow.loadAssets()
+      .then(function() {
+        ctrl.assets = Cashflow.getAssets();
+      }, function(err) {
+        console.log(err);
+      });
+
+    ctrl.createChart();
+  };
 
   ctrl.asset = {};
 
@@ -19,6 +23,23 @@ function CashflowController($scope, $element, $attrs, Cashflow) {
         console.log(err);
       });
   };
+
+  ctrl.createChart = function() {
+    var element = _.first($element.find('.ct-chart'));
+
+    new Chartist.Line(element, {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8],
+      series: [
+        [5, 9, 7, 8, 5, 3, 5, 4],
+        [3, 8, 4, 6, 2, 1, 4, 2]
+      ]
+    }, {
+      low: 0,
+      showArea: true,
+      showPoint: false,
+      fullWIdth: true,
+    });
+  }
 }
 
 angular.module('app').component('cashflow', {
